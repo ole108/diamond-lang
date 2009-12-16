52a502084f694ffc6a9cea1ee29d64997e2d4161
ole108 <ole@diamond-lang.org>
2009-12-17 00:10:25 +0100

    Added test for srcbuf package

--- go/src/diamondlang/srcbuf/srcbuf_test.go
+++ go/src/diamondlang/srcbuf/srcbuf_test.go
@@ -16,9 +16,91 @@ func TestAscii(t *testing.T) {
     }
   }
   if sb.Getch() != common.EOF {
-    t.Error("ASCII character not recognized.");
+    t.Error("EOF not recognized.");
   }
 }
 
-func TestNonAscii(t *testing.T) {
+func TestAtStartOfLine(t *testing.T) {
+  tstBuf := []byte{ 'a', '\n', 'b', '\n', 'c' };
+  sb := NewSourceFromBuffer(tstBuf, "TestAtStartOfLine");
+  if !sb.AtStartOfLine() {
+    t.Error("Start of buffer isn't start of line.");
+  }
+  sb.Getch();  // got 'a'
+  if !sb.AtStartOfLine() {
+    t.Error("Start of first line isn't start of line.");
+  }
+  sb.Getch();  // got '\n'
+  if sb.AtStartOfLine() {
+    t.Error("Getch() should move away from start of line.");
+  }
+  sb.Getch();  // got 'b'
+  if !sb.AtStartOfLine() {
+    t.Error("Start of next line not recognized.");
+  }
+  sb.NotAtStartOfLine();
+  if sb.AtStartOfLine() {
+    t.Error("NotAtStartOfLine() didn't work.");
+  }
+  sb.Getch();  // got '\n'
+  sb.Getch();  // got 'c'
+  if !sb.AtStartOfLine() {
+    t.Error("Start of last line not recognized.");
+  }
+  if sb.Getch() != common.EOF {
+    t.Error("EOF not recognized.");
+  }
 }
+
+func TestLineEndings(t *testing.T) {
+  tstBuf := []byte{ 'a', '\n', 'b', '\r', '\n', 'c', '\r', 'd', '\n', '\r', 'e' };
+  sb := NewSourceFromBuffer(tstBuf, "TestLineEndings");
+  sb.Getch();  // got 'a'
+  if !sb.AtStartOfLine() {
+    t.Error("Start of 1. line not recognized.");
+  }
+  sb.Getch();  // got '\n'
+  if sb.AtStartOfLine() {
+    t.Error("Getch() should move away from start of line.");
+  }
+  sb.Getch();  // got 'b'
+  if !sb.AtStartOfLine() {
+    t.Error("Start of 2. line not recognized.");
+  }
+  sb.Getch();  // got '\r'
+  if sb.AtStartOfLine() {
+    t.Error("'\\r' isn't start of line.");
+  }
+  sb.Getch();  // got '\n'
+  if sb.AtStartOfLine() {
+    t.Error("'\\n' isn't start of line.");
+  }
+  sb.Getch();  // got 'c'
+  if !sb.AtStartOfLine() {
+    t.Error("Start of 3. line not recognized.");
+  }
+  sb.Getch();  // got '\r'
+  if sb.AtStartOfLine() {
+    t.Error("'\\r' isn't start of line (2).");
+  }
+  sb.Getch();  // got 'd'
+  if !sb.AtStartOfLine() {
+    t.Error("Start of 4. line not recognized.");
+  }
+  sb.Getch();  // got '\n'
+  if sb.AtStartOfLine() {
+    t.Error("'\\n' isn't start of line.");
+  }
+  sb.Getch();  // got '\r'
+  if !sb.AtStartOfLine() {
+    t.Error("Start of 5. line not recognized.");
+  }
+  sb.Getch();  // got 'e'
+  if !sb.AtStartOfLine() {
+    t.Error("Start of 6. line not recognized.");
+  }
+  if sb.Getch() != common.EOF {
+    t.Error("EOF not recognized.");
+  }
+}
+
